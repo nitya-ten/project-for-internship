b933c33c56558abc8684c124470feae7945eb279
Nityanand kumar <2276531@cognizant.com>
2023-05-12 16:10:36 +0530

    added property service

---
+++ property-service/src/main/java/com/nitya/rest/propertyservice/entity/Property.java
@@ -0,0 +1,78 @@
+package com.nitya.rest.propertyservice.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class Property {
+	
+	@Id
+	@GeneratedValue
+	private Integer id;
+	
+	private String title;
+	private String address;
+	private String description;
+	private Integer userId;
+	
+	public Property() {
+		
+	}
+	
+	public Property(String title, String address, String description, Integer userId) {
+		super();
+		this.title = title;
+		this.address = address;
+		this.description = description;
+		this.userId = userId;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Integer getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Integer userId) {
+		this.userId = userId;
+	}
+
+	@Override
+	public String toString() {
+		return "Property [id=" + id + ", title=" + title + ", address=" + address + ", description=" + description
+				+ ", userId=" + userId + "]";
+	}
+	
+	
+}
---
+++ property-service/src/main/java/com/nitya/rest/propertyservice/repository/PropertyRepository.java
@@ -0,0 +1,23 @@
+package com.nitya.rest.propertyservice.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.nitya.rest.propertyservice.entity.Property;
+
+import jakarta.transaction.Transactional;
+
+@Transactional
+public interface PropertyRepository extends JpaRepository<Property, Integer>{
+
+	Optional<Property> findByUserId(Integer userId);
+
+	void deleteByUserId(Integer userId);
+
+	Optional<List<Property>> findAllByUserId(Integer userId);
+
+	void deleteAllByUserId(Integer userId);
+
+}
